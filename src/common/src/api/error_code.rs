261f59d1aeaf260842151530390eebbc4532a9af
bruderj15 <julian.bruder@outlook.com>
2025-09-09 10:30:34 +0200

    feat#80: extract user-id from cognito-jwt

--- src/common/src/api/error_code.rs
+++ src/common/src/api/error_code.rs
@@ -24,6 +24,8 @@ pub const BAD_PAGE_SIZE_VALUE: ApiErrorCode = ApiErrorCode("BAD_PAGE_SIZE_VALUE"
 pub const BAD_SORT_VALUE: ApiErrorCode = ApiErrorCode("BAD_SORT_VALUE");
 pub const BAD_ORDER_VALUE: ApiErrorCode = ApiErrorCode("BAD_ORDER_VALUE");
 
+pub const INVALID_UUID: ApiErrorCode = ApiErrorCode("INVALID_UUID");
+
 pub const ITEM_NOT_FOUND: ApiErrorCode = ApiErrorCode("ITEM_NOT_FOUND");
 pub const SEARCH_FILTER_NOT_FOUND: ApiErrorCode = ApiErrorCode("SEARCH_FILTER_NOT_FOUND");
 pub const MONETARY_AMOUNT_OVERFLOW: ApiErrorCode = ApiErrorCode("MONETARY_AMOUNT_OVERFLOW");
--- src/common/src/user_id.rs
+++ src/common/src/user_id.rs
@@ -50,3 +50,184 @@ impl TryFrom<&str> for UserId {
         Uuid::parse_str(s).map(Self)
     }
 }
+
+#[cfg(feature = "api")]
+pub mod api {
+    use crate::{
+        api::{
+            error::ApiError,
+            error_code::{INTERNAL_SERVER_ERROR, UNAUTHORIZED},
+        },
+        user_id::UserId,
+    };
+    use aws_lambda_events::apigw::ApiGatewayV2httpRequestContext;
+
+    pub fn extract_user_id_cognito_jwt(
+        request_context: &ApiGatewayV2httpRequestContext,
+    ) -> Result<UserId, ApiError> {
+        let user_id = request_context
+            .authorizer
+            .as_ref()
+            .ok_or_else(|| {
+                ApiError::unauthorized(UNAUTHORIZED)
+                    .with_header_field("Authorization")
+                    .with_message("Missing authorizer-information in request-context.")
+            })?
+            .jwt
+            .as_ref()
+            .ok_or_else(|| ApiError::unauthorized(UNAUTHORIZED).with_header_field("Authorization").with_message("Missing JWT."))?
+            .claims
+            .get("sub")
+            .ok_or_else(|| {
+                tracing::error!("Missing claim 'sub' in Cognito-authorized JWT.");
+                ApiError::internal_server_error(INTERNAL_SERVER_ERROR)
+            })
+            .map(String::as_str)
+            .map(UserId::try_from)?
+            .map_err(|err| {
+                tracing::error!(eror = %err, "Claim 'sub' in Cognito-authorized JWT is not a valid UUID.");
+                ApiError::internal_server_error(INTERNAL_SERVER_ERROR)
+            })?;
+
+        Ok(user_id)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::user_id::api::extract_user_id_cognito_jwt;
+        use aws_lambda_events::apigw::{
+            ApiGatewayRequestAuthorizer, ApiGatewayRequestAuthorizerJwtDescription,
+            ApiGatewayV2httpRequestContext,
+        };
+        use std::collections::HashMap;
+        use uuid::Uuid;
+
+        #[test]
+        fn should_extract_user_id() {
+            let expected = Uuid::new_v4();
+            let request_context = ApiGatewayV2httpRequestContext {
+                route_key: Default::default(),
+                account_id: Default::default(),
+                stage: Default::default(),
+                request_id: Default::default(),
+                authorizer: Some(ApiGatewayRequestAuthorizer {
+                    jwt: Some(ApiGatewayRequestAuthorizerJwtDescription {
+                        claims: HashMap::from([("sub".to_string(), expected.to_string())]),
+                        scopes: None,
+                    }),
+                    fields: Default::default(),
+                    iam: None,
+                }),
+                apiid: Default::default(),
+                domain_name: Default::default(),
+                domain_prefix: Default::default(),
+                time: Default::default(),
+                time_epoch: Default::default(),
+                http: Default::default(),
+                authentication: Default::default(),
+            };
+
+            let actual = extract_user_id_cognito_jwt(&request_context).unwrap().0;
+
+            assert_eq!(expected, actual);
+        }
+
+        #[test]
+        fn should_401_when_authorizer_information_missing() {
+            let request_context = ApiGatewayV2httpRequestContext::default();
+
+            let actual = extract_user_id_cognito_jwt(&request_context).unwrap_err();
+
+            assert_eq!(401, actual.status);
+            assert_eq!("UNAUTHORIZED", actual.error.as_str());
+        }
+
+        #[test]
+        fn should_401_when_jwt_missing() {
+            let request_context = ApiGatewayV2httpRequestContext {
+                route_key: Default::default(),
+                account_id: Default::default(),
+                stage: Default::default(),
+                request_id: Default::default(),
+                authorizer: Some(ApiGatewayRequestAuthorizer {
+                    jwt: None,
+                    fields: Default::default(),
+                    iam: None,
+                }),
+                apiid: Default::default(),
+                domain_name: Default::default(),
+                domain_prefix: Default::default(),
+                time: Default::default(),
+                time_epoch: Default::default(),
+                http: Default::default(),
+                authentication: Default::default(),
+            };
+
+            let actual = extract_user_id_cognito_jwt(&request_context).unwrap_err();
+
+            assert_eq!(401, actual.status);
+            assert_eq!("UNAUTHORIZED", actual.error.as_str());
+        }
+
+        #[test]
+        fn should_500_when_claim_sub_missing() {
+            let request_context = ApiGatewayV2httpRequestContext {
+                route_key: Default::default(),
+                account_id: Default::default(),
+                stage: Default::default(),
+                request_id: Default::default(),
+                authorizer: Some(ApiGatewayRequestAuthorizer {
+                    jwt: Some(ApiGatewayRequestAuthorizerJwtDescription::default()),
+                    fields: Default::default(),
+                    iam: None,
+                }),
+                apiid: Default::default(),
+                domain_name: Default::default(),
+                domain_prefix: Default::default(),
+                time: Default::default(),
+                time_epoch: Default::default(),
+                http: Default::default(),
+                authentication: Default::default(),
+            };
+
+            let actual = extract_user_id_cognito_jwt(&request_context).unwrap_err();
+
+            assert_eq!(500, actual.status);
+            assert_eq!("INTERNAL_SERVER_ERROR", actual.error.as_str());
+        }
+
+        #[rstest::rstest]
+        #[case("")]
+        #[case("boop")]
+        #[case("foo")]
+        #[case("4bf40051-84cc-4ebf-898c")]
+        fn should_500_when_claim_sub_is_not_valid_uuid(#[case] sub: String) {
+            let request_context = ApiGatewayV2httpRequestContext {
+                route_key: Default::default(),
+                account_id: Default::default(),
+                stage: Default::default(),
+                request_id: Default::default(),
+                authorizer: Some(ApiGatewayRequestAuthorizer {
+                    jwt: Some(ApiGatewayRequestAuthorizerJwtDescription {
+                        claims: HashMap::from_iter([("sub".to_string(), sub)]),
+                        scopes: None,
+                    }),
+                    fields: Default::default(),
+                    iam: None,
+                }),
+                apiid: Default::default(),
+                domain_name: Default::default(),
+                domain_prefix: Default::default(),
+                time: Default::default(),
+                time_epoch: Default::default(),
+                http: Default::default(),
+                authentication: Default::default(),
+            };
+
+            let actual = extract_user_id_cognito_jwt(&request_context).unwrap_err();
+
+            assert_eq!(500, actual.status);
+            assert_eq!("INTERNAL_SERVER_ERROR", actual.error.as_str());
+        }
+    }
+}
