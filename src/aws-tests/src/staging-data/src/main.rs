c0fd45c331a1bcfaaca8c9e5e59786d79e742c78
bruderj15 <julian.bruder@outlook.com>
2025-10-03 11:41:19 +0200

    task#139: enhance fake item-history

--- src/aws-tests/src/staging-data/src/main.rs
+++ src/aws-tests/src/staging-data/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use aws_tests_common::get_cfn_output;
 use common::api::collection::PutCollectionData;
 use fake::{Fake, Faker};
@@ -9,6 +11,7 @@ use staging_tests::{get_dynamodb_client, get_opensearch_client};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    staging_tests::reset().await;
     populate_items().await;
     populate_shops().await;
 
@@ -19,8 +22,7 @@ async fn populate_items() {
     let stack = get_cfn_output();
     let put_items_url = format!("{}/api/v1/items", stack.api_gateway_endpoint_url);
 
-    staging_tests::reset().await;
-
+    // create items
     let put_item_commands = PutCollectionData {
         items: fake::vec![PutItemData; 142],
     };
@@ -31,24 +33,25 @@ async fn populate_items() {
         .await
         .unwrap();
     assert_eq!(200, response.status());
+    tokio::time::sleep(Duration::from_secs(30)).await;
 
-    let mut updates = put_item_commands
-        .items
-        .into_iter()
-        .take(75)
-        .collect::<Vec<_>>();
-    for put_item in &mut updates {
-        put_item.state = Faker.fake();
-        put_item.price = Some(Faker.fake());
+    // put updates
+    for _ in 0..7 {
+        let mut updates = put_item_commands.items.to_vec();
+        for put_item in &mut updates {
+            put_item.state = Faker.fake();
+            put_item.price = Faker.fake();
+        }
+        let put_commands = PutCollectionData { items: updates };
+        let response = reqwest::Client::new()
+            .put(&put_items_url)
+            .json(&put_commands)
+            .send()
+            .await
+            .unwrap();
+        assert_eq!(200, response.status());
+        tokio::time::sleep(Duration::from_secs(30)).await;
     }
-    let put_update_commands = PutCollectionData { items: updates };
-    let response = reqwest::Client::new()
-        .put(&put_items_url)
-        .json(&put_update_commands)
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(200, response.status());
 }
 
 async fn populate_shops() {
