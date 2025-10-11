b303aacd9ef57be58dd46c86647ee3920889e1fd
bruderj15 <julian.bruder@outlook.com>
2025-10-11 21:33:05 +0200

    task#156: add basic types

---
+++ src/user/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod command;
+pub mod repository;
+pub mod service;
+pub mod user;
+pub mod user_record;
---
+++ src/user/src/user.rs
@@ -0,0 +1,41 @@
+use common::user_id::UserId;
+use serde_email::Email;
+use time::OffsetDateTime;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct User {
+    pub id: UserId,
+    pub email: Email,
+    pub created: OffsetDateTime,
+    pub updated: OffsetDateTime,
+}
+
+#[cfg(feature = "test-data")]
+mod fake {
+    use crate::user::User;
+    use fake::{Fake, faker::internet::de_de::SafeEmail};
+    use time::OffsetDateTime;
+
+    impl fake::Dummy<fake::Faker> for User {
+        fn dummy_with_rng<R: fake::rand::Rng + ?Sized>(config: &fake::Faker, rng: &mut R) -> Self {
+            let email_str: String = SafeEmail().fake_with_rng(rng);
+            User {
+                id: config.fake_with_rng(rng),
+                email: email_str.try_into().unwrap(),
+                created: OffsetDateTime::now_utc(),
+                updated: OffsetDateTime::now_utc(),
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::user::User;
+        use fake::{Fake, Faker};
+
+        #[test]
+        fn should_fake_user() {
+            let _ = Faker.fake::<User>();
+        }
+    }
+}
