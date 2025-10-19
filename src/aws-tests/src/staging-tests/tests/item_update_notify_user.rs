241ed37e550aab9c9ec8fe67471f0e143b569ccf
bruderj15 <julian.bruder@outlook.com>
2025-10-19 10:46:18 +0200

    feat#165: much more liberal wait

--- src/aws-tests/src/staging-tests/tests/item_update_notify_user.rs
+++ src/aws-tests/src/staging-tests/tests/item_update_notify_user.rs
@@ -50,7 +50,7 @@ async fn should_send_email_to_user_when_watched_item_has_update() {
         .await
         .unwrap();
     assert_eq!(200, response.status());
-    tokio::time::sleep(Duration::from_secs(2)).await;
+    tokio::time::sleep(Duration::from_secs(30)).await;
 
     // create user
     let user = create_test_user(
@@ -82,7 +82,7 @@ async fn should_send_email_to_user_when_watched_item_has_update() {
         .await
         .unwrap();
     assert_eq!(201, post_response.status());
-    tokio::time::sleep(Duration::from_secs(2)).await;
+    tokio::time::sleep(Duration::from_secs(3)).await;
 
     // enable notifications
     let patch_url = format!(
