6b95e99341ff35ec43395ca2f7699bfbe354b948
bruderj15 <julian.bruder@outlook.com>
2025-11-05 16:41:45 +0100

    feat#87: specify and implement `ItemPersonalizationService`

--- src/item/src/service/mod.rs
+++ src/item/src/service/mod.rs
@@ -7,5 +7,9 @@ pub mod enrichment_service;
 #[cfg(feature = "dynamodb")]
 pub mod get_service;
 pub mod item_command;
+
+#[cfg(all(feature = "watchlist", feature = "dynamodb"))]
+pub mod personalization_service;
+
 #[cfg(feature = "opensearch")]
 pub mod query_service;
