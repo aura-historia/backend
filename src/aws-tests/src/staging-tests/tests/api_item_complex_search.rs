618d363bfcafc53d6f0a9ad2dc39bf34c8371c16
bruderj15 <julian.bruder@outlook.com>
2025-11-07 21:03:32 +0100

    feat#187 align complex-search staging-tests

--- src/aws-tests/src/staging-tests/tests/api_item_complex_search.rs
+++ src/aws-tests/src/staging-tests/tests/api_item_complex_search.rs
@@ -7,22 +7,143 @@ use common::{
 use fake::{Fake, Faker};
 use item::data::item_search_data::ItemSearchData;
 use item::data::item_state_data::ItemStateData;
+use item::dynamodb::repository::ItemDynamoDbRepositoryImpl;
 use item::opensearch::{
     item_document::ItemDocument,
     item_state_document::ItemStateDocument,
     repository::{ItemOpenSearchRepository, ItemOpenSearchRepositoryImpl},
 };
+use item::service::get_service::GetItemServiceImpl;
+use item::watchlist::dynamodb::repository::WatchlistItemDynamoDbRepositoryImpl;
+use item::watchlist::service::item_watchlist_service::{
+    ItemWatchListService, ItemWatchListServiceImpl,
+};
 use opensearch::{IndexParts, params::Refresh};
-use staging_tests::{get_opensearch_client, staging_test};
+use staging_tests::{
+    create_random_test_user, get_dynamodb_client, get_opensearch_client, staging_test,
+};
 use std::{
     time::{Duration, SystemTime},
     vec,
 };
 use time::macros::datetime;
 use url::Url;
+use user::dynamodb::repository::UserDynamoDbRepositoryImpl;
 
 #[staging_test]
-async fn should_respond_200_when_hits() {
+async fn should_respond_200_when_hits_authenticated() {
+    let cfn = get_cfn_output();
+    let dynamodb_client = get_dynamodb_client().await;
+    let user_repository =
+        UserDynamoDbRepositoryImpl::new(dynamodb_client, &cfn.dynamodb_table_1_name);
+    let watchlist_repository =
+        WatchlistItemDynamoDbRepositoryImpl::new(dynamodb_client, &cfn.dynamodb_table_1_name);
+    let item_repository =
+        ItemDynamoDbRepositoryImpl::new(dynamodb_client, &cfn.dynamodb_table_1_name);
+    let get_item_service = GetItemServiceImpl::new(&item_repository);
+    let item_watchlist_service = ItemWatchListServiceImpl::new(
+        &watchlist_repository,
+        &user_repository,
+        &item_repository,
+        &get_item_service,
+    );
+
+    let os_client = get_opensearch_client().await;
+    let item_opensearch_repository = ItemOpenSearchRepositoryImpl::new(os_client);
+    let expected = ItemDocument {
+        item_id: ItemId::new(),
+        event_id: EventId::new(),
+        shop_id: ShopId::new(),
+        shops_item_id: ShopsItemId::new(),
+        shop_name: "Hans Volkers Shop".into(),
+        title_de: Some("Chopin Etudes Op.10 1833".to_string()),
+        title_en: None,
+        description_de: None,
+        description_en: None,
+        price_eur: Some(1400000),
+        price_usd: Some(1500000),
+        price_gbp: Some(1600000),
+        price_aud: Some(1700000),
+        price_cad: Some(1800000),
+        price_nzd: Some(1990000),
+        state: ItemStateDocument::Available,
+        url: Url::parse("https://hans-volker.com/chopin-etudes-op10-1833").unwrap(),
+        images: vec![],
+        embedding: None,
+        created: SystemTime::now().into(),
+        updated: SystemTime::now().into(),
+    };
+    let mut all = fake::vec![ItemDocument; 10];
+    all.push(expected.clone());
+
+    let insert_res = item_opensearch_repository
+        .create_item_documents(all)
+        .await
+        .unwrap();
+    assert!(!insert_res.errors);
+    os_client
+        .index(IndexParts::Index("items"))
+        .refresh(Refresh::True)
+        .send()
+        .await
+        .unwrap();
+    tokio::time::sleep(Duration::from_secs(3)).await;
+
+    let user = create_random_test_user().await;
+    item_watchlist_service
+        .create_watchlist_item(&user.sub.into(), &expected.shop_id, &expected.shops_item_id)
+        .await
+        .unwrap();
+
+    let search_filter = ItemSearchData {
+        language: LanguageData::De,
+        currency: CurrencyData::Eur,
+        item_query: "Chopin Etudes Op.10".try_into().unwrap(),
+        shop_name_query: Some("Hans Volkers".try_into().unwrap()),
+        price_query: Some(RangeQuery {
+            min: None,
+            max: Some(99999999),
+        }),
+        state_query: [ItemStateData::Available, ItemStateData::Listed].into(),
+        created_query: Some(RangeQuery {
+            min: None,
+            max: Some(datetime!(2999 - 01 - 02 0:00 UTC)),
+        }),
+        updated_query: None,
+    };
+
+    let url = format!(
+        "{}/api/v1/items/search?sort=created&order=asc&size=5",
+        get_cfn_output().api_gateway_endpoint_url
+    );
+    let response = reqwest::Client::new()
+        .post(url)
+        .json(&search_filter)
+        .send()
+        .await
+        .unwrap();
+    assert_eq!(200, response.status());
+
+    let body = response.json::<serde_json::Value>().await.unwrap();
+    assert_eq!(1, body["size"]);
+    assert_eq!(1, body["total"]);
+
+    let item = body["items"].as_array().unwrap()[0]["item"].clone();
+    assert_eq!(expected.shop_id.to_string(), item["shopId"]);
+    assert_eq!(expected.shops_item_id.to_string(), item["shopsItemId"]);
+    assert_eq!(expected.item_id.to_string(), item["itemId"]);
+    assert_eq!(expected.event_id.to_string(), item["eventId"]);
+    assert_eq!(expected.url.to_string(), item["url"]);
+    assert_eq!(expected.price_eur.unwrap(), item["price"]["amount"]);
+    assert_eq!("EUR", item["price"]["currency"]);
+
+    let user_state = body["items"].as_array().unwrap()[0]["userState"].clone();
+    assert!(user_state["watchlist"]["watching"].as_bool().unwrap());
+    assert!(!user_state["watchlist"]["notifications"].as_bool().unwrap());
+}
+
+#[staging_test]
+async fn should_respond_200_when_hits_anon() {
     let os_client = get_opensearch_client().await;
     let repository = ItemOpenSearchRepositoryImpl::new(os_client);
     let expected = ItemDocument {
@@ -94,7 +215,7 @@ async fn should_respond_200_when_hits() {
     assert_eq!(1, body["size"]);
     assert_eq!(1, body["total"]);
 
-    let item = body["items"].as_array().unwrap()[0].clone();
+    let item = body["items"].as_array().unwrap()[0]["item"].clone();
     assert_eq!(expected.shop_id.to_string(), item["shopId"]);
     assert_eq!(expected.shops_item_id.to_string(), item["shopsItemId"]);
     assert_eq!(expected.item_id.to_string(), item["itemId"]);
@@ -102,10 +223,11 @@ async fn should_respond_200_when_hits() {
     assert_eq!(expected.url.to_string(), item["url"]);
     assert_eq!(expected.price_eur.unwrap(), item["price"]["amount"]);
     assert_eq!("EUR", item["price"]["currency"]);
+    assert!(body["items"].as_array().unwrap()[0]["userState"].is_null());
 }
 
 #[staging_test]
-async fn should_respond_200_when_no_hits() {
+async fn should_respond_200_when_no_hits_anon() {
     let url = format!(
         "{}/api/v1/items/search",
         get_cfn_output().api_gateway_endpoint_url
