c199d62dbfe1074464e4f20ae714011c275657cc
bruderj15 <julian.bruder@outlook.com>
2025-11-14 18:39:01 +0100

    task#198: remove noisy logs

--- src/product-enrichment/src/main.rs
+++ src/product-enrichment/src/main.rs
@@ -23,7 +23,6 @@ static AWS_CONFIG: OnceCell<SdkConfig> = OnceCell::const_new();
 pub async fn get_aws_config() -> &'static SdkConfig {
     AWS_CONFIG
         .get_or_init(|| async {
-            info!("Loading AWS-Config...");
             aws_config::defaults(BehaviorVersion::v2025_08_07())
                 .load()
                 .await
@@ -34,20 +33,14 @@ pub async fn get_aws_config() -> &'static SdkConfig {
 static DYNAMODB_CLIENT: OnceCell<aws_sdk_dynamodb::Client> = OnceCell::const_new();
 pub async fn get_dynamodb_client() -> &'static aws_sdk_dynamodb::Client {
     DYNAMODB_CLIENT
-        .get_or_init(|| async {
-            info!("Loading DynamoDB-Client...");
-            aws_sdk_dynamodb::Client::new(get_aws_config().await)
-        })
+        .get_or_init(|| async { aws_sdk_dynamodb::Client::new(get_aws_config().await) })
         .await
 }
 
 static SQS_CLIENT: OnceCell<aws_sdk_sqs::Client> = OnceCell::const_new();
 pub async fn get_sqs_client() -> &'static aws_sdk_sqs::Client {
     SQS_CLIENT
-        .get_or_init(|| async {
-            info!("Loading SQS-Client...");
-            aws_sdk_sqs::Client::new(get_aws_config().await)
-        })
+        .get_or_init(|| async { aws_sdk_sqs::Client::new(get_aws_config().await) })
         .await
 }
 
@@ -55,13 +48,8 @@ static OPENSEARCH_CLIENT: OnceCell<opensearch::OpenSearch> = OnceCell::const_new
 pub async fn get_opensearch_client() -> &'static opensearch::OpenSearch {
     OPENSEARCH_CLIENT
         .get_or_init(|| async {
-            info!("Loading OpenSearch-Client...");
             let domain_endpoint = std::env::var("OPENSEARCH_DOMAIN_ENDPOINT_URL")
                 .expect("shouldn't fail reading env-var 'OPENSEARCH_DOMAIN_ENDPOINT_URL'");
-            info!(
-                url = domain_endpoint,
-                "Loaded OpenSearch Domain-Endpoint-Url."
-            );
             let domain_endpoint_url = Url::parse(&domain_endpoint).expect(
                 "shouldn't fail parsing value for env-var 'OPENSEARCH_DOMAIN_ENDPOINT_URL' as url",
             );
@@ -93,39 +81,25 @@ async fn main() {
 
     let table_name = std::env::var("DYNAMODB_TABLE_NAME")
         .expect("shouldn't fail reading env-var 'DYNAMODB_TABLE_NAME'");
-    info!(tableName = table_name, "Loaded DynamoDb table-name.");
     let product_dynamodb_repository =
         ProductDynamoDbRepositoryImpl::new(get_dynamodb_client().await, &table_name);
-    info!(type = %std::any::type_name::<ProductDynamoDbRepositoryImpl>(), "Loaded component.");
 
     let enrichment_queue_url = std::env::var("ENRICHMENT_QUEUE_URL")
         .expect("shouldn't fail reading env-var 'ENRICHMENT_QUEUE_URL'");
-    info!(
-        queueUrl = enrichment_queue_url,
-        "Loaded EnrichmentQueue-Url."
-    );
     let sqs_client_arc = Arc::new(get_sqs_client().await.clone());
 
     let product_opensearch_repository =
         ProductOpenSearchRepositoryImpl::new(get_opensearch_client().await);
-    info!(type = %std::any::type_name::<ProductOpenSearchRepositoryImpl>(), "Loaded component.");
-
     let faucet =
         EnrichmentPipeFaucetImpl::new(sqs_client_arc.clone(), enrichment_queue_url.clone());
-    info!(type = %std::any::type_name::<EnrichmentPipeFaucetImpl>(), "Loaded component.");
-
     let embedding_delegate = EmbeddingDelegateImpl::new().unwrap();
-    info!(type = %std::any::type_name::<EmbeddingDelegateImpl>(), "Loaded component.");
-
     let embedding_pipe = EmbeddingEnrichmentPipeImpl::new(Arc::new(embedding_delegate));
-    info!(type = %std::any::type_name::<EmbeddingEnrichmentPipeImpl>(), "Loaded component.");
     let pipes: Vec<Box<dyn EnrichmentPipe + Send + Sync>> = vec![Box::new(embedding_pipe)];
 
     let sink = EnrichmentPipeSinkImpl::new(
         Arc::new(product_dynamodb_repository),
         Arc::new(product_opensearch_repository),
     );
-    info!(type = %std::any::type_name::<EnrichmentPipeSinkImpl>(), "Loaded component.");
 
     let plumbing = EnrichmentPlumbingImpl::new(
         Arc::new(faucet),
@@ -134,7 +108,6 @@ async fn main() {
         sqs_client_arc,
         enrichment_queue_url,
     );
-    info!(type = %std::any::type_name::<EnrichmentPlumbingImpl>(), "Loaded component.");
 
     info!("Initialization complete.");
     loop {
