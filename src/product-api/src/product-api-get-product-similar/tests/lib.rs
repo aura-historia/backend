4d09438bcd81c27f46c3a688fccae012e4a9503f
bruderj15 <julian.bruder@outlook.com>
2025-12-08 18:15:13 +0100

    task#235: fixes #263

--- src/product-api/src/product-api-get-product-similar/tests/lib.rs
+++ src/product-api/src/product-api-get-product-similar/tests/lib.rs
@@ -1461,7 +1461,7 @@ async fn should_respond_200_and_respect_accept_language_header(
         .unwrap();
     assert!(!os_insert_res.errors);
     refresh_index("products").await;
-    tokio::time::sleep(Duration::from_secs(10)).await;
+    tokio::time::sleep(Duration::from_secs(3)).await;
 
     let lambda_event = LambdaEvent {
         payload: ApiGatewayV2httpRequestProxy::builder()
@@ -1486,8 +1486,6 @@ async fn should_respond_200_and_respect_accept_language_header(
     let actual: Vec<PersonalizedData<GetProductData, ProductUserStateData>> =
         serde_json::from_value(response_payload).unwrap();
 
-    // tough due to ANN
-    assert!(1 < actual.len());
     assert!(
         actual
             .iter()
