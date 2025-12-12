0ede78e81ff50d20948eea4800d9757a0ac82d96
bruderj15 <julian.bruder@outlook.com>
2025-12-12 20:07:40 +0100

    feat#256: human-readable `ProductState`

--- src/common/src/product_state/domain.rs
+++ src/common/src/product_state/domain.rs
@@ -1,5 +1,7 @@
 use strum_macros::EnumCount;
 
+use crate::language::domain::Language;
+
 #[cfg_attr(feature = "test-data", derive(fake::Dummy))]
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Hash, EnumCount)]
 pub enum ProductState {
@@ -9,4 +11,47 @@ pub enum ProductState {
     Sold,
     Removed,
     Unknown,
+}
+
+impl ProductState {
+    pub fn format_human_readable(&self, language: &Language) -> &'static str {
+        match self {
+            ProductState::Listed => match language {
+                Language::De => "Gelistet",
+                Language::En => "Listed",
+                Language::Fr => "Listé",
+                Language::Es => "Listado",
+            },
+            ProductState::Available => match language {
+                Language::De => "Verfügbar",
+                Language::En => "Available",
+                Language::Fr => "Disponible",
+                Language::Es => "Disponible",
+            },
+            ProductState::Reserved => match language {
+                Language::De => "Reserviert",
+                Language::En => "Reserved",
+                Language::Fr => "Réservé",
+                Language::Es => "Reservado",
+            },
+            ProductState::Sold => match language {
+                Language::De => "Verkauft",
+                Language::En => "Sold",
+                Language::Fr => "Vendu",
+                Language::Es => "Vendido",
+            },
+            ProductState::Removed => match language {
+                Language::De => "Gelöscht",
+                Language::En => "Removed",
+                Language::Fr => "Supprimé",
+                Language::Es => "Eliminado",
+            },
+            ProductState::Unknown => match language {
+                Language::De => "Unbekannt",
+                Language::En => "Unknown",
+                Language::Fr => "Inconnu",
+                Language::Es => "Desconocido",
+            },
+        }
+    }
 }
