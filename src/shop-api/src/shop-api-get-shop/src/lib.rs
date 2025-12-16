4d94b7ae852a52a35c4012799c4e3c1256082418
bruderj15 <julian.bruder@outlook.com>
2025-12-16 21:10:01 +0100

    feat#292: make shop-api accept shop-domain as identifier next to shop-id

--- src/shop-api/src/shop-api-get-shop/src/lib.rs
+++ src/shop-api/src/shop-api-get-shop/src/lib.rs
@@ -1,9 +1,9 @@
 use aws_lambda_events::apigw::{ApiGatewayV2httpRequest, ApiGatewayV2httpResponse};
 use common::api::api_gateway_v2_http_response_builder::ApiGatewayV2HttpResponseBuilder;
 use common::api::error::{ApiError, log_api_error};
-use common::shop_id::api::extract_shop_id_path;
 use lambda_runtime::LambdaEvent;
 use shop::data::get_shop_data::GetShopData;
+use shop::data::shop_identifier_data::extract_shop_identifier_data_path;
 use shop::service::get_service::GetShopService;
 
 #[tracing::instrument(
@@ -31,14 +31,14 @@ pub async fn handler(
     }
 }
 
-// GET /api/v1/shops/{shopId}
+// GET /api/v1/shops/{shopIdentifier}
 pub async fn handle(
     event: LambdaEvent<ApiGatewayV2httpRequest>,
     service: &impl GetShopService,
 ) -> Result<ApiGatewayV2httpResponse, ApiError> {
-    let shop_id = extract_shop_id_path(&event.payload.path_parameters)?;
+    let shop_identifier = extract_shop_identifier_data_path(&event.payload.path_parameters)?.into();
 
-    let shop_data: GetShopData = service.find_shop(&shop_id).await?.into();
+    let shop_data: GetShopData = service.find_shop(&shop_identifier).await?.into();
 
     Ok(ApiGatewayV2HttpResponseBuilder::json(200)
         .last_modified(shop_data.updated)
@@ -59,7 +59,7 @@ mod tests {
     use time::macros::datetime;
 
     #[tokio::test]
-    async fn should_include_updated_timestamp_as_header_last_modified() {
+    async fn should_include_updated_timestamp_as_header_last_modified_for_shop_id() {
         let timestamp = datetime!(2020-01-01 0:00 UTC);
         let mut service = MockGetShopService::default();
         service.expect_find_shop().return_once(move |_| {
@@ -71,7 +71,31 @@ mod tests {
         let lambda_event = LambdaEvent {
             payload: ApiGatewayV2httpRequestProxy::builder()
                 .http_method(http::Method::GET)
-                .path_parameter("shopId", shop_id)
+                .path_parameter("shopIdentifier", shop_id)
+                .build(),
+            context: Default::default(),
+        };
+        let response = handler(lambda_event, &service).await.unwrap();
+        assert_eq!(200, response.status_code);
+        assert_eq!(
+            "Wed, 01 Jan 2020 00:00:00 GMT",
+            response.headers.get(LAST_MODIFIED).unwrap()
+        );
+    }
+
+    #[tokio::test]
+    async fn should_include_updated_timestamp_as_header_last_modified_for_shop_domain() {
+        let timestamp = datetime!(2020-01-01 0:00 UTC);
+        let mut service = MockGetShopService::default();
+        service.expect_find_shop().return_once(move |_| {
+            let mut shop: Shop = Faker.fake();
+            shop.updated = timestamp;
+            Box::pin(async move { Ok(shop) })
+        });
+        let lambda_event = LambdaEvent {
+            payload: ApiGatewayV2httpRequestProxy::builder()
+                .http_method(http::Method::GET)
+                .path_parameter("shopIdentifier", "foo.bar")
                 .build(),
             context: Default::default(),
         };
@@ -98,7 +122,7 @@ mod tests {
         assert_eq!(400, response.status_code);
         let json = extract_apigw_response_json_body!(response);
         assert_eq!(400, json["status"]);
-        assert_eq!("shopId", json["source"]["field"]);
+        assert_eq!("shopIdentifier", json["source"]["field"]);
     }
 
     #[tokio::test]
@@ -107,16 +131,18 @@ mod tests {
         let lambda_event = LambdaEvent {
             payload: ApiGatewayV2httpRequestProxy::builder()
                 .http_method(http::Method::GET)
-                .path_parameter("shopId", shop_id)
+                .path_parameter("shopIdentifier", shop_id)
                 .build(),
             context: Default::default(),
         };
 
         let mut service = MockGetShopService::default();
-        service.expect_find_shop().return_once(move |shop_id| {
-            let shop_id = *shop_id;
-            Box::pin(async move { Err(GetShopError::ShopNotFound(shop_id)) })
-        });
+        service
+            .expect_find_shop()
+            .return_once(move |shop_identifier| {
+                let shop_identifier = shop_identifier.clone();
+                Box::pin(async move { Err(GetShopError::ShopNotFound(shop_identifier)) })
+            });
 
         let response = handler(lambda_event, &service).await.unwrap();
         assert_eq!(404, response.status_code);
