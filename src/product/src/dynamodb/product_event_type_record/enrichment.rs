ca54286cf131dee63f09b028249b8e6b36a40f4e
Julian Bruder <julian.bruder@outlook.com>
2026-01-31 22:11:15 +0100

    epic#458: add new product-event-type-records

---
+++ src/product/src/dynamodb/product_event_type_record/enrichment.rs
@@ -0,0 +1,31 @@
+use crate::core::product_event::enrichment::ProductEnrichmentEventPayload;
+use serde::{Deserialize, Serialize};
+
+#[cfg_attr(feature = "test-data", derive(fake::Dummy))]
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ProductEnrichmentEventTypeRecord {
+    EnrichmentTranslatedTitle,
+    EnrichmentTranslatedDescription,
+    EnrichmentEmbeddedText,
+    EnrichmentExtractedAttributes,
+}
+
+impl From<&ProductEnrichmentEventPayload> for ProductEnrichmentEventTypeRecord {
+    fn from(domain: &ProductEnrichmentEventPayload) -> Self {
+        match domain {
+            ProductEnrichmentEventPayload::TranslatedTitle(_) => {
+                ProductEnrichmentEventTypeRecord::EnrichmentTranslatedTitle
+            }
+            ProductEnrichmentEventPayload::TranslatedDescription(_) => {
+                ProductEnrichmentEventTypeRecord::EnrichmentTranslatedDescription
+            }
+            ProductEnrichmentEventPayload::EmbeddedText(_) => {
+                ProductEnrichmentEventTypeRecord::EnrichmentEmbeddedText
+            }
+            ProductEnrichmentEventPayload::ExtractedAttributes(_) => {
+                ProductEnrichmentEventTypeRecord::EnrichmentExtractedAttributes
+            }
+        }
+    }
+}
