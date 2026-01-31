003afd6cdff7cc321ef4e5139568b27baa5c3f6b
Julian Bruder <julian.bruder@outlook.com>
2026-01-31 20:02:03 +0100

    epic#458: add `ProductPolicyEvent`

--- src/product/src/core/product_event/mod.rs
+++ src/product/src/core/product_event/mod.rs
@@ -1,6 +1,11 @@
-use crate::core::product_event::domain::ProductDomainEventPayload;
+use crate::core::product_event::{
+    domain::ProductDomainEventPayload, policy::ProductPolicyEventPayload,
+};
 use common::{event::Event, product_id::ProductId};
 
 pub mod domain;
+pub mod enrichment;
+pub mod policy;
 
 pub type ProductDomainEvent = Event<ProductId, ProductDomainEventPayload>;
+pub type ProductPolicyEvent = Event<ProductId, ProductPolicyEventPayload>;
---
+++ src/product/src/core/product_event/policy.rs
@@ -0,0 +1,18 @@
+use crate::core::prohibited_content::ProhibitedContent;
+use common::{reason::Reason, shop_id::ShopId, shops_product_id::ShopsProductId};
+
+#[cfg_attr(feature = "test-data", derive(fake::Dummy))]
+#[derive(Debug, Clone, PartialEq)]
+#[allow(clippy::large_enum_variant)]
+pub enum ProductPolicyEventPayload {
+    ProhibitedContent(ProhibitedContentProductPolicyEventPayload),
+}
+
+#[cfg_attr(feature = "test-data", derive(fake::Dummy))]
+#[derive(Debug, Clone, PartialEq)]
+pub struct ProhibitedContentProductPolicyEventPayload {
+    pub shop_id: ShopId,
+    pub shops_product_id: ShopsProductId,
+    pub decision: ProhibitedContent,
+    pub reason: Reason,
+}
