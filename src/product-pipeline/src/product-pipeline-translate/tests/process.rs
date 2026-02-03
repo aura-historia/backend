971e800007cb5af3b9f90fe2407945360a9957ed
Julian Bruder <julian.bruder@outlook.com>
2026-02-03 18:53:34 +0100

    epic#458: align translate to enterpris-memory

--- src/product-pipeline/src/product-pipeline-translate/tests/process.rs
+++ src/product-pipeline/src/product-pipeline-translate/tests/process.rs
@@ -1,47 +1,31 @@
 use common::language::domain::Language;
-use common::language::record::LanguageRecord;
-use common::{language::record::TextRecord, product_id::ProductId};
-use fake::rand::seq::SliceRandom;
+use common::localized::Localized;
+use common::product_id::ProductId;
 use fake::{Fake, Faker};
-use product_pipeline_common::{process::PipeProcessor, types::CleansedPipeProduct};
+use product::core::product::Product;
+use product_pipeline_common::process::PipeProcessor;
 use product_pipeline_translate::{
     adapter::TranslationAdapterImpl, process::TranslationPipeProcesserImpl,
 };
 use std::sync::Arc;
-use strum::EnumCount;
 
-#[rstest::rstest]
-#[trace]
-#[case(0)]
+#[test]
 #[serial_test::serial]
-fn should_process_translation(#[case] count: usize) {
+fn should_process_translation() {
     let adapter = TranslationAdapterImpl::new().unwrap();
     let translation_pipe_processor = TranslationPipeProcesserImpl::new(Arc::new(adapter));
 
     let product_id = ProductId::new();
-    let mut products = fake::vec![CleansedPipeProduct; count];
-    let mut product = Faker.fake::<CleansedPipeProduct>();
+    let mut product = Faker.fake::<Product>();
     product.product_id = product_id;
-    product.native_title.text = "Test".to_owned();
-    product.native_title.language = LanguageRecord::De;
-    product.native_description = Some(TextRecord {
-        language: LanguageRecord::De,
-        text: "Beispiel".to_owned(),
+    product.native_title.payload = "Test".into();
+    product.native_title.localization = Language::De;
+    product.native_description = Some(Localized {
+        localization: Language::De,
+        payload: "Beispiel".into(),
     });
-    products.push(product);
-    products.shuffle(&mut fake::rand::rng());
 
-    let actual = translation_pipe_processor.process(products);
+    let actual = translation_pipe_processor.process(vec![product]);
     assert!(actual.failures.is_empty());
-    assert_eq!(count + 1, actual.successes.len());
-
-    let translated = actual
-        .successes
-        .into_iter()
-        .find(|out_product| out_product.product_id == product_id)
-        .unwrap();
-
-    assert_eq!(Language::COUNT - 1, translated.other_title.len(),);
-    assert_eq!("Test", translated.native_title.text);
-    assert_eq!(LanguageRecord::De, translated.native_title.language);
+    assert_eq!(6, actual.successes.len());
 }
