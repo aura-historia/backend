0001757f9c6f52123383f48c414378c2a55478e5
Julian Bruder <julian.bruder@outlook.com>
2026-02-07 22:55:41 +0100

    feat#493: add macro generating string-newtypes

---
+++ src/common/src/string_newtype.rs
@@ -0,0 +1,75 @@
+#[macro_export]
+macro_rules! string_newtype {
+    // Case with serde enabled
+    ($name:ident, serde) => {
+        #[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Hash, serde::Serialize, serde::Deserialize)]
+        #[serde(transparent)]
+        pub struct $name(String);
+
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
+
+        impl From<$name> for String {
+            fn from(value: $name) -> Self {
+                value.0
+            }
+        }
+
+        impl From<String> for $name {
+            fn from(value: String) -> Self {
+                $name(value)
+            }
+        }
+
+        impl From<&String> for $name {
+            fn from(value: &String) -> Self {
+                $name(value.to_owned())
+            }
+        }
+
+        impl From<&str> for $name {
+            fn from(value: &str) -> Self {
+                $name(value.to_owned())
+            }
+        }
+    };
+
+    // Case without serde
+    ($name:ident) => {
+        #[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Hash)]
+        pub struct $name(String);
+
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
+
+        impl From<$name> for String {
+            fn from(value: $name) -> Self {
+                value.0
+            }
+        }
+
+        impl From<String> for $name {
+            fn from(value: String) -> Self {
+                $name(value)
+            }
+        }
+
+        impl From<&String> for $name {
+            fn from(value: &String) -> Self {
+                $name(value.to_owned())
+            }
+        }
+
+        impl From<&str> for $name {
+            fn from(value: &str) -> Self {
+                $name(value.to_owned())
+            }
+        }
+    };
+}
