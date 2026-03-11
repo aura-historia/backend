a219e08422bfebaba01ae9ca14897b990178c5ca
Julian Bruder <julian.bruder@outlook.com>
2026-03-11 22:57:07 +0100

    task#574: align cfn to notification & email changes

--- src/mail/src/mail-core/src/lib.rs
+++ src/mail/src/mail-core/src/lib.rs
@@ -2,16 +2,9 @@ pub mod mail_id;
 pub mod payload;
 pub mod template;
 
-#[cfg(feature = "queue")]
-pub mod queue_service;
-
 #[cfg(feature = "send")]
 pub mod record;
 #[cfg(feature = "send")]
-pub mod repository;
-#[cfg(feature = "send")]
 pub mod s3_adapter;
-#[cfg(feature = "send")]
-pub mod send_service;
 #[cfg(feature = "send")]
 pub mod ses_adapter;
