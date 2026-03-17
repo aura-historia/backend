8699b1a6c98571678e45dc7ae9e8704b87204a23
Julian Bruder <julian.bruder@outlook.com>
2026-03-17 17:45:17 +0100

    feat#618: add `ProhibitedContentUserState`

--- src/product/src/core/user_state.rs
+++ src/product/src/core/user_state.rs
@@ -1,10 +1,16 @@
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
 pub struct ProductUserState {
     pub watchlist: WatchlistUserState,
+    pub prohibited_content: ProhibitedContentUserState,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
 pub struct WatchlistUserState {
     pub watching: bool,
     pub notifications: bool,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub struct ProhibitedContentUserState {
+    pub consent: bool,
 }
