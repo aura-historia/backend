b96f35996b1739cd586dfeff97f72517856ee053
Julian Bruder <julian.bruder@outlook.com>
2026-05-09 18:06:42 +0200

    task#1006: remove dead queues

--- src/test-api/src/cloudformation.rs
+++ src/test-api/src/cloudformation.rs
@@ -194,8 +194,6 @@ impl IntegrationTestService for Cloudformation {
             cfn.product_pipeline_embed_text_queue_url.clone(),
             cfn.product_pipeline_embed_text_dead_letter_queue_url
                 .clone(),
-            cfn.product_pipeline_classify_queue_url.clone(),
-            cfn.product_pipeline_classify_dead_letter_queue_url.clone(),
         ])
         .await;
         debug!("Drained all SQS queues for test isolation");
